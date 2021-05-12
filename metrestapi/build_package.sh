93ec3b9c3108005d4bf90343f73b45dd461b2ac7
crouchr <richard.crouch100@gmail.com>
2021-05-12 22:17:42 +0100

    Improved package installation script

--- metrestapi/build_package.sh
+++ metrestapi/build_package.sh
@@ -5,4 +5,5 @@
 
 pipenv run python setup.py bdist_wheel
 pipenv run python setup.py sdist
-scp dist/* crouchr@192.168.1.5://home/crouchr/PycharmProjects/learnage/environments/production/web-server/apache/python-packages/metrestapi/
+cp dist/* /home/crouchr/PycharmProjects/learnage/environments/production/web-server/apache/python-packages/metrestapi/
+#scp dist/* crouchr@192.168.1.5://home/crouchr/PycharmProjects/learnage/environments/production/web-server/apache/python-packages/metrestapi/
